3ad08e6b497b895e7c0aef94b36fdd7f5ade537b
RomanAlxndrvch <heyekzo@gmail.com>
2023-08-13 18:08:57 -0600

    fix imports

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import mongoose, {ConnectOptions} from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config({path: "./config.env"});
-import {app} from "./app";
+import {app} from "app";
 
 const DB = process.env.DATABASE?.replace(
     "<PASSWORD>",
